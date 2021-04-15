c599ec4a9e21056162a28c19b55a95837f4c66e6
ZhuGYao <1031487079@qq.com>
2021-04-15 14:42:58 +0800

    test code

---
+++ src/main/java/com/zgy/develop/annotation/db/Column.java
@@ -0,0 +1,19 @@
+package com.zgy.develop.annotation.db;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author zgy
+ * @data 2021/4/15 14:36
+ */
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Column {
+
+    String value() default "";
+
+}
