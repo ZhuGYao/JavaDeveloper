de0065a3000c15a68e575528f0f535ae0c9f9e82
ZhuGYao <1031487079@qq.com>
2021-04-19 17:20:16 +0800

    添加自定义注解

---
+++ src/main/java/com/zgy/develop/annotation/custom/CosmoController.java
@@ -0,0 +1,9 @@
+package com.zgy.develop.annotation.custom;
+
+/**
+ * @author zgy
+ * @data 2021/4/19 17:16
+ */
+
+public @interface CosmoController {
+}
---
+++ src/main/java/com/zgy/develop/common/enums/CustomWeekDayEnum.java
@@ -0,0 +1,9 @@
+package com.zgy.develop.common.enums;
+
+/**
+ * @author zgy
+ * @data 2021/4/18 13:29
+ */
+
+public class CustomWeekDayEnum {
+}
