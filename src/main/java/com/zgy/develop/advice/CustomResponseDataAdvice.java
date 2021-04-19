aa35e6c520014b3e2559c270ce5bd7494c5c98d6
ZhuGYao <1031487079@qq.com>
2021-04-19 17:38:45 +0800

    ...

---
+++ src/main/java/com/zgy/develop/advice/CustomResponseDataAdvice.java
@@ -0,0 +1,9 @@
+package com.zgy.develop.advice;
+
+/**
+ * @author zgy
+ * @data 2021/4/19 17:22
+ */
+
+public class CustomResponseDataAdvice {
+}
---
+++ src/main/java/com/zgy/develop/controller/TestController.java
@@ -0,0 +1,9 @@
+package com.zgy.develop.controller;
+
+/**
+ * @author zgy
+ * @data 2021/4/19 17:21
+ */
+
+public class TestController {
+}
