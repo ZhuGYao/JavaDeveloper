3f911f633384bfcd472d01764fe3ca82bc576d6d
ZhuGYao <1031487079@qq.com>
2021-04-25 16:22:34 +0800

    add junit

--- src/main/java/com/zgy/develop/juint/MyAnnotationTest.java
+++ src/main/java/com/zgy/develop/juint/MyAnnotationTest.java
@@ -1,13 +1,18 @@
-package com.zgy.annotation;
+package com.zgy.develop.juint;
 
-import com.zgy.enums.TypeEnum;
+import com.zgy.develop.annotation.unit.MyAfter;
+import com.zgy.develop.annotation.unit.MyBefore;
+import com.zgy.develop.annotation.unit.MyTest;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 朱光耀
  * @data 2021/4/14 20:02
  */
 
-@MyAnnotation(value = "shit", type = TypeEnum.NORMAL)
 public class MyAnnotationTest {
 
     @MyBefore
@@ -20,8 +25,42 @@ public class MyAnnotationTest {
         System.out.println("test");
     }
 
+    @MyTest
+    public void test2() {
+        System.out.println("test2");
+    }
+
     @MyAfter
     public void after() {
         System.out.println("after");
+    }
+
+    public static void main(String[] args)  throws Exception {
+        Class<MyAnnotationTest> myAnnotationTestClass = MyAnnotationTest.class;
+        MyAnnotationTest myAnnotationTest = myAnnotationTestClass.newInstance();
+
+        Method[] methods = myAnnotationTestClass.getMethods();
+        List<Method> beforeList = new ArrayList<>();
+        List<Method> afterList = new ArrayList<>();
+        List<Method> testList = new ArrayList<>();
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(MyBefore.class)) {
+                beforeList.add(m);
+            } else if (m.isAnnotationPresent(MyAfter.class)) {
+                afterList.add(m);
+            } else if (m.isAnnotationPresent(MyTest.class)) {
+                testList.add(m);
+            }
+        }
+
+        for (Method m : testList) {
+            for (Method b : beforeList) {
+                b.invoke(myAnnotationTest);
+            }
+            m.invoke(myAnnotationTest);
+            for (Method a : afterList) {
+                a.invoke(myAnnotationTest);
+            }
+        }
     }
 }
