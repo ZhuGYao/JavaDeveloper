cefec03195d12e15a12fd7b2600bd7a5fd9ada2d
ZhuGYao <1031487079@qq.com>
2021-04-26 23:53:48 +0800

    add spring

---
+++ src/main/java/com/zgy/develop/spring/web/CustomDispatchServlet.java
@@ -0,0 +1,39 @@
+package com.zgy.develop.spring.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * 请求总口分发servlet
+ * @author zgy
+ * @data 2021/4/26 19:29
+ */
+
+public class MyDispatchServlet extends HttpServlet {
+
+    private Class<?> clazz;
+    private Properties properties;
+
+    public MyDispatchServlet(Class<?> clazz, Properties properties)
+    {
+        this.clazz = clazz;
+        this.properties = properties;
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        this.doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+
+    }
+}
