c3c97f522f8cd9be6a47e44392b495c8272c96f3
ZhuGYao <1031487079@qq.com>
2021-05-10 20:05:10 +0800

    modify

---
+++ src/main/java/com/zgy/develop/rbac/dao/RolePermissionMergeDao.java
@@ -0,0 +1,9 @@
+package com.zgy.develop.rbac.dao;
+
+/**
+ * @author zgy
+ * @data 2021/5/10 19:55
+ */
+
+public class RolePermissionMergeDao {
+}
---
+++ src/main/java/com/zgy/develop/rbac/dao/UserRoleMergeDao.java
@@ -0,0 +1,9 @@
+package com.zgy.develop.rbac.dao;
+
+/**
+ * @author zgy
+ * @data 2021/5/10 19:56
+ */
+
+public class UserRoleMergeDao {
+}
