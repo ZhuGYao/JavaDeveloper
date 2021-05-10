1d1aa9d60addfb36ed317d07fe40b7a869f15f6d
ZhuGYao <1031487079@qq.com>
2021-05-10 16:40:18 +0800

    modify

---
+++ src/main/java/com/zgy/develop/annotation/rbac/PermissionRequired.java
@@ -0,0 +1,9 @@
+package com.zgy.develop.annotation.rbac;
+
+/**
+ * @author zgy
+ * @data 2021/5/10 16:13
+ */
+
+public @interface PermissionRequired {
+}
---
+++ src/main/java/com/zgy/develop/rbac/enums/Logical.java
@@ -0,0 +1,9 @@
+package com.zgy.develop.rbac.enums;
+
+/**
+ * @author zgy
+ * @data 2021/5/10 16:14
+ */
+
+public enum Logical {
+}
