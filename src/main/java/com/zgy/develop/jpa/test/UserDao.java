8b0ffa8efa5865b0e2d6261fa6b5a3137018acd8
ZhuGYao <1031487079@qq.com>
2021-05-16 23:55:32 +0800

    ...

--- src/main/java/com/zgy/develop/jpa/test/UserDao.java
+++ src/main/java/com/zgy/develop/jpa/test/UserDao.java
@@ -27,26 +27,25 @@ public class UserDao extends MySQLBaseDao<User> {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         // FutureTask实现了Runnable，可以看做是一个任务
-        FutureTask<String> futureTask = new FutureTask<>(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                System.out.println(Thread.currentThread().getName() + "========>正在执行");
-                try {
-                    Thread.sleep(3 * 1000L);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                return "success";
-            }
-        });
-
-        System.out.println(Thread.currentThread().getName() + "========>启动任务");
-
-        // 传入futureTask，启动线程执行任务
-        new Thread(futureTask).start();
-
-        futureTask.isDone();
-        // 但它同时又实现了Future，可以获取异步结果（会阻塞3秒）
+//        FutureTask<String> futureTask = new FutureTask<>(new Callable<String>() {
+//            @Override
+//            public String call() throws Exception {
+//                System.out.println(Thread.currentThread().getName() + "========>正在执行");
+//                try {
+//                    Thread.sleep(3 * 1000L);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                return "success";
+//            }
+//        });
+//
+//        System.out.println(Thread.currentThread().getName() + "========>启动任务");
+//
+//        // 传入futureTask，启动线程执行任务
+//        new Thread(futureTask).start();
+//
+//        futureTask.isDone();
 //        刚创建
 //        即将完成
 //        完成
