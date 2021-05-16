04ba368f07c0936dde2a644615db5c67ee07d3f2
ZhuGYao <1031487079@qq.com>
2021-05-16 23:48:14 +0800

    .....

--- src/main/java/com/zgy/develop/jpa/test/UserDao.java
+++ src/main/java/com/zgy/develop/jpa/test/UserDao.java
@@ -10,6 +10,8 @@ import java.util.concurrent.atomic.LongAdder;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.LockSupport;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 /**
  * @author zgy
@@ -57,6 +59,8 @@ public class UserDao extends MySQLBaseDao<User> {
 
 
 
+        ArrayBlockingQueue<String> arrayBlockingQueue = new ArrayBlockingQueue<>(10);
+        LinkedBlockingQueue<String> linkedBlockingQueue = new LinkedBlockingQueue<>(10);
 //
 //        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
         ReentrantLock reentrantLock = new ReentrantLock();
@@ -70,6 +74,31 @@ public class UserDao extends MySQLBaseDao<User> {
 //        list.add("111");
 //        Long l = Long.valueOf(list.size());
 //        System.out.println(l);
+
+        // 提交一个任务，返回CompletableFuture（注意，并不是把CompletableFuture提交到线程池，它没有实现Runnable）
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(new Supplier<String>() {
+            @Override
+            public String get() {
+                System.out.println("=============>异步线程开始...");
+                System.out.println("=============>异步线程为：" + Thread.currentThread().getName());
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("=============>异步线程结束...");
+                return "supplierResult";
+            }
+        });
+
+        // 异步回调：上面的Supplier#get()返回结果后，异步线程会回调BiConsumer#accept()
+        completableFuture.whenComplete(new BiConsumer<String, Throwable>() {
+            @Override
+            public void accept(String s, Throwable throwable) {
+                System.out.println("=============>异步任务结束回调...");
+                System.out.println("=============>回调线程为：" + Thread.currentThread().getName());
+            }
+        });
     }
 
 }
