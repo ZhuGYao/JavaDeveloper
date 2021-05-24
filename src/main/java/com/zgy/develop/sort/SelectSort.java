659a816b9abf93239808c2b530f21804c183f0c0
ZhuGYao <1031487079@qq.com>
2021-05-24 22:44:06 +0800

    ...

--- src/main/java/com/zgy/develop/sort/SelectSort.java
+++ src/main/java/com/zgy/develop/sort/SelectSort.java
@@ -6,4 +6,23 @@ package com.zgy.develop.sort;
  */
 
 public class SelectSort {
+
+    public static void sort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr, minIndex, i);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
