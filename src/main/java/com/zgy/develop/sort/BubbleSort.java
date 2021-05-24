f6846b3ddf35b651718b011c500f6014fcd3d45b
ZhuGYao <1031487079@qq.com>
2021-05-24 22:43:16 +0800

    ...

--- src/main/java/com/zgy/develop/sort/BubbleSort.java
+++ src/main/java/com/zgy/develop/sort/BubbleSort.java
@@ -6,4 +6,21 @@ package com.zgy.develop.sort;
  */
 
 public class BubbleSort {
+
+    public static void sort(int[] arr) {
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
