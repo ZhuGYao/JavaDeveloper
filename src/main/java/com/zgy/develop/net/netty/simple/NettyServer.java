f6d85ba4b0d960e85fa54be4218c99ad2d4b72fe
ZhuGYao <1031487079@qq.com>
2021-06-05 23:50:41 +0800

    modify netty

--- src/main/java/com/zgy/develop/net/netty/simple/NettyServer.java
+++ src/main/java/com/zgy/develop/net/netty/simple/NettyServer.java
@@ -21,22 +21,27 @@ public class NettyServer {
         // 创建两个线程组
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            // 创建服务启动对象,配置参数
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(new NettyServerHandler());
+                        }
+                    });
+            // 绑定端口同步
+            ChannelFuture cf = bootstrap.bind(8888).sync();
+            // 对关闭通道事件进行监听
+            cf.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
 
-        // 创建服务启动对象,配置参数
-        ServerBootstrap bootstrap = new ServerBootstrap();
-        bootstrap.group(bossGroup, workerGroup)
-                .channel(NioServerSocketChannel.class)
-                .option(ChannelOption.SO_BACKLOG, 128)
-                .childOption(ChannelOption.SO_KEEPALIVE, true)
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel ch) {
-                        ch.pipeline().addLast(null);
-                    }
-                });
-        // 绑定端口同步
-        ChannelFuture cf = bootstrap.bind(8888).sync();
-        // 对关闭通道事件进行监听
-        cf.channel().closeFuture().sync();
     }
 }
---
+++ src/main/java/com/zgy/develop/net/netty/simple/NettyServerHandler.java
@@ -0,0 +1,36 @@
+package com.zgy.develop.net.netty.simple;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author zgy
+ * @data 2021/6/5 22:09
+ */
+
+@Slf4j
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf buf = (ByteBuf) msg;
+        log.info("{}:{}",ctx.channel().remoteAddress(), buf.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        String msg = "hello world";
+        ctx.writeAndFlush(Unpooled.copiedBuffer(msg, CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
