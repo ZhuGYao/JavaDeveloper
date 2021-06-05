289c997025844c84574a38d936962286b5039fe3
ZhuGYao <1031487079@qq.com>
2021-06-05 23:57:37 +0800

    add netty client

---
+++ src/main/java/com/zgy/develop/net/netty/simple/NettyClient.java
@@ -0,0 +1,13 @@
+package com.zgy.develop.net.netty.simple;
+
+/**
+ * @author zgy
+ * @data 2021/6/5 23:56
+ */
+
+public class NettyClient {
+
+    public static void main(String[] args) {
+
+    }
+}
