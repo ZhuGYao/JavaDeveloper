52cfe71b41c9d7acd15ca22eee0f53cfeecf620d
ZhuGYao <1031487079@qq.com>
2021-06-14 00:51:36 +0800

    modify http server

--- src/main/java/com/zgy/develop/net/netty/http/HttpServer.java
+++ src/main/java/com/zgy/develop/net/netty/http/HttpServer.java
@@ -1,9 +1,39 @@
 package com.zgy.develop.net.netty.http;
 
+import com.zgy.develop.net.netty.simple.NettyServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author zgy
  * @data 2021/6/8 22:57
  */
 
+@Slf4j
 public class HttpServer {
+
+    public static void main(String[] args) throws Exception {
+        // 创建两个线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            // 创建服务启动对象,配置参数
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(null);
+            // 绑定端口同步
+            ChannelFuture cf = bootstrap.bind(8888).sync();
+
+            // 对关闭通道事件进行监听
+            cf.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
 }
--- src/main/java/com/zgy/develop/net/netty/http/HttpServerInitializer.java
+++ src/main/java/com/zgy/develop/net/netty/http/HttpServerInitializer.java
@@ -1,9 +1,19 @@
 package com.zgy.develop.net.netty.http;
 
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author zgy
  * @data 2021/6/8 23:30
  */
 
-public class HttpServerInitializer {
+@Slf4j
+public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+    }
 }
