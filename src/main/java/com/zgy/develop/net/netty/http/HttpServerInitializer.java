66cce28599c4b04ccecd25ff67dbad97703713d6
ZhuGYao <1031487079@qq.com>
2021-06-14 01:34:46 +0800

    modify http server

--- src/main/java/com/zgy/develop/net/netty/http/HttpServerInitializer.java
+++ src/main/java/com/zgy/develop/net/netty/http/HttpServerInitializer.java
@@ -1,7 +1,9 @@
 package com.zgy.develop.net.netty.http;
 
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -14,6 +16,11 @@ public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
 
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+        // 向管道加入处理器
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        // netty 提供的http编解码器
+        pipeline.addLast("MyHttpServerCodec", new HttpServerCodec());
 
     }
 }
