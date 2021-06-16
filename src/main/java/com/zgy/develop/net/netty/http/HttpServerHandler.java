eedd4a2aa957412185b3dbea34dedeb1d69da6f5
ZhuGYao <1031487079@qq.com>
2021-06-16 23:17:03 +0800

    modify http server

--- src/main/java/com/zgy/develop/net/netty/http/HttpServerHandler.java
+++ src/main/java/com/zgy/develop/net/netty/http/HttpServerHandler.java
@@ -1,9 +1,18 @@
 package com.zgy.develop.net.netty.http;
 
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.HttpObject;
+
 /**
  * @author zgy
  * @data 2021/6/8 22:59
  */
 
-public class HttpServerHandler {
+public class HttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
+
+    }
 }
