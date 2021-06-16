1b19ee065929a2fabc367fd003e097c579b1cece
ZhuGYao <1031487079@qq.com>
2021-06-16 23:55:36 +0800

    modify http server

--- src/main/java/com/zgy/develop/net/netty/http/HttpServerHandler.java
+++ src/main/java/com/zgy/develop/net/netty/http/HttpServerHandler.java
@@ -1,10 +1,15 @@
 package com.zgy.develop.net.netty.http;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.HttpObject;
-import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 
 /**
  * @author zgy
@@ -19,6 +24,13 @@ public class HttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
         if (msg instanceof HttpRequest) {
             log.info("client address : {}", ctx.channel().remoteAddress());
+            // 返回内容
+            ByteBuf byteBuf = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
+            DefaultFullHttpResponse httpResponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, byteBuf);
+            // 设置请求头
+            httpResponse.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            httpResponse.headers().set(HttpHeaderNames.CONTENT_LENGTH, byteBuf.readableBytes());
+
         }
     }
 }
