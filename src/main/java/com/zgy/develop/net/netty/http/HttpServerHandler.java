9494e43723c9f1713ce4b8e9d1f6034bcd856afd
ZhuGYao <1031487079@qq.com>
2021-06-16 23:44:25 +0800

    modify http server

--- src/main/java/com/zgy/develop/net/netty/http/HttpServerHandler.java
+++ src/main/java/com/zgy/develop/net/netty/http/HttpServerHandler.java
@@ -3,16 +3,22 @@ package com.zgy.develop.net.netty.http;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpRequest;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author zgy
  * @data 2021/6/8 22:59
  */
 
+@Slf4j
 public class HttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
 
+        if (msg instanceof HttpRequest) {
+            log.info("client address : {}", ctx.channel().remoteAddress());
+        }
     }
 }
