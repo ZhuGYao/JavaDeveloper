49837e30a4b3eea252666b10ec3f589958d425ab
ZhuGYao <1031487079@qq.com>
2021-06-26 23:37:25 +0800

    modify http server

--- src/main/java/com/zgy/develop/net/netty/buf/NettyByteBuffer.java
+++ src/main/java/com/zgy/develop/net/netty/buf/NettyByteBuffer.java
@@ -2,15 +2,19 @@ package com.zgy.develop.net.netty.buf;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class NettyByteBuffer {
 
     public static void main(String[] args) {
 
-//        test1();
+        test2();
     }
 
     public static void test1() {
+
         ByteBuf buffer = Unpooled.buffer(10);
 
         for (int i = 0; i < buffer.capacity(); i++) {
@@ -25,5 +29,11 @@ public class NettyByteBuffer {
 
     public static void test2() {
 
+        ByteBuf byteBuf = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
+
+        if (byteBuf.hasArray()) {
+            byte[] array = byteBuf.array();
+            log.info(new String(array, CharsetUtil.UTF_8));
+        }
     }
 }
